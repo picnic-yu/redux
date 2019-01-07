ff4444bad3a0f6396f8b9a879073e0a74644af14
picnic-yu <164005206@qq.com>
2019-01-07 21:13:15 +0800

    create store

--- src/index.js
+++ src/index.js
@@ -6,17 +6,6 @@ function Button(){
     )
 }
 
-
-let appState = {
-    title:{
-        color:'red',
-        text:'标题1'
-    },
-    content:{
-        color:'green',
-        text: '标题2'
-    }
-}
 function renderTitle(title) {
     let element = document.querySelector('#title');
     element.innerHTML = title.text;
@@ -27,11 +16,96 @@ function renderContent(content){
     element.innerHTML = content.text;
     element.style.color = content.color;
 }
-function renderApp(appState) {
-    renderTitle(appState.title);
-    renderContent(appState.content)
+function renderApp(state) {
+    renderTitle(state.title);
+    renderContent(state.content)
 }
-renderApp(appState)
+const UPDATE_TITLE_COLOR = 'UPDATE_TITLE_COLOR'
+const UPDATE_TITLE_TEXT = 'UPDATE_TITLE_TEXT'
+const UPDATE_CONTENT_COLOR = 'UPDATE_CONTENT_COLOR'
+const UPDATE_CONTENT_TEXT = 'UPDATE_CONTENT_TEXT'
+/**
+ * 创建一个仓库
+ */
+function createStore(){
+    let state = {
+        title: {
+            color: 'red',
+            text: '标题1'
+        },
+        content: {
+            color: 'green',
+            text: '标题2'
+        }
+    }
+    function getState(){
+        // 深拷贝
+        return JSON.parse(JSON.stringify(state))
+    }
+    /**
+     * 
+     * @param {*} action 
+     */
+    // 接收一个action动作 描述你想干啥
+    function dispatch(action) {
+        switch (action.type) {
+            case 'UPDATE_TITLE_COLOR':
+                state.title.color = action.color;
+                break;
+            case 'UPDATE_TITLE_TEXT':
+                state.title.text = action.text;
+                break;
+            case 'UPDATE_CONTENT_COLOR':
+                state.content.color = action.color;
+                break;
+            case 'UPDATE_CONTENT_TEXT':
+                state.content.text = action.text;
+                break;
+            default:
+                break;
+        }
+    }
+    return{
+        getState,
+        dispatch
+    }
+} 
+let store = createStore();
+renderApp(store.getState());
+setTimeout(()=>{
+    store.dispatch({
+        type: UPDATE_TITLE_COLOR,
+        color: 'skyblue'
+    })
+    store.dispatch({
+        type: UPDATE_TITLE_TEXT,
+        text: 'skyblue'
+    })
+    renderApp(store.getState());
+},2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ReactDOM.render(
     <Button />,document.getElementById('root') 
 )
